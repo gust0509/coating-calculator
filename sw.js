cc22a2df8fb02f0cfe9535163aa06d11a67191f2
Alvin <gust0509@yahoo.com.tw>
2026-04-17 12:45:09 +0800

    Bump SW cache to v4 for forced refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'coating-calc-v3';
+const CACHE_NAME = 'coating-calc-v4';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
